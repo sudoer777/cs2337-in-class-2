354b411d6c703d9a1219390586175c4e1175b717
sudoer777 <78781902+sudoer777@users.noreply.github.com>
2022-10-04 10:07:43 -0500

    Create rectangle class

---
+++ Rectangle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ethan on 10/4/22.
+//
+
+#include "Rectangle.h"
---
+++ Rectangle.h
@@ -0,0 +1,14 @@
+//
+// Created by ethan on 10/4/22.
+//
+
+#ifndef IN_CLASS_2_RECTANGLE_H
+#define IN_CLASS_2_RECTANGLE_H
+
+
+class Rectangle {
+
+};
+
+
+#endif //IN_CLASS_2_RECTANGLE_H
