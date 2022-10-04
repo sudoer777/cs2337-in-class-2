31702ca7f751fea11ed4243279e2783b437242c6
Drakatoa <54646819+Drakatoa@users.noreply.github.com>
2022-10-04 10:23:14 -0500

    Update Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,27 +1,61 @@
 //
 // Created by ethan on 10/4/22.
 //
-
+#include <iostream>
+#include <math.h>
 #include "Rectangle.h"
 
-Rectangle Rectangle::operator+(const Rectangle &rectangle) const {
-    return Rectangle();
-}
+const float epsilon = 0.000001f;
 
 Rectangle::Rectangle(double length, double width) {
 
 }
 
-Rectangle Rectangle::operator-(const Rectangle &rectangle) const {
-    return Rectangle();
+Rectangle Rectangle::operator+(Rectangle rs){
+    Rectangle final;
+    
+    final.length=length+rs.length;
+    final.width=width+rs.width;
+    
+    return final;
 }
 
-bool Rectangle::operator<(const Rectangle &rectangle) const {
-    return false;
+Rectangle Rectangle::operator-(Rectangle rs){
+    Rectangle final;
+    
+    final.length=length-rs.length;
+    if(final.length<0){
+        final.length=0;
+    }
+    final.width=width-rs.width;
+    if(final.width<0){
+        final.width=0;
+    }
+    
+    return final;
 }
 
-bool Rectangle::operator>(const Rectangle &rectangle) const {
-    return false;
+bool Rectangle::operator<(Rectangle rs){
+    double lhs = length*width;
+    double rhs = rs.length*rs.width;
+    if(fabs(lhs-rhs)<epsilon){
+        return false;
+    }else if(lhs<rhs){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool Rectangle::operator>(Rectangle rs){
+    double lhs = length*width;
+    double rhs = rs.length*rs.width;
+    if(fabs(lhs-rhs)<epsilon){
+        return false;
+    }else if(lhs>rhs){
+        return true;
+    }else{
+        return false;
 }
 
 bool Rectangle::operator==(const Rectangle &rectangle) const {
