f034f9928b302f795466d3d64cb3770aa413f613
sudoer777 <78781902+sudoer777@users.noreply.github.com>
2022-10-04 10:20:22 -0500

    Create header stuff

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "Rectangle.h"
+
+Rectangle Rectangle::operator+(const Rectangle &rectangle) const {
+    return Rectangle();
+}
+
+Rectangle::Rectangle(double length, double width) {
+
+}
+
+Rectangle Rectangle::operator-(const Rectangle &rectangle) const {
+    return Rectangle();
+}
+
+bool Rectangle::operator<(const Rectangle &rectangle) const {
+    return false;
+}
+
+bool Rectangle::operator>(const Rectangle &rectangle) const {
+    return false;
+}
+
+bool Rectangle::operator==(const Rectangle &rectangle) const {
+    return false;
+}
+
+std::ostream &operator<<(std::ostream &output, const Rectangle &rectangle) {
+    return <#initializer#>;
+}
+
+Rectangle::Rectangle(const Rectangle &rectangle) {
+
+}
+
+Rectangle &Rectangle::operator++() {
+    return <#initializer#>;
+}
+
+Rectangle Rectangle::operator++(int) {
+    return Rectangle();
+}
--- Rectangle.h
+++ Rectangle.h
@@ -5,9 +5,29 @@
 #ifndef IN_CLASS_2_RECTANGLE_H
 #define IN_CLASS_2_RECTANGLE_H
 
+#include <ostream>
+#include <istream>
 
 class Rectangle {
-
+public:
+    Rectangle operator+(const Rectangle &rectangle) const;
+    Rectangle operator-(const Rectangle &rectangle) const;
+    bool operator<(const Rectangle &rectangle) const;
+    bool operator>(const Rectangle &rectangle) const;
+    bool operator==(const Rectangle &rectangle) const;
+    Rectangle &operator++();
+    Rectangle operator++(int);
+    friend std::ostream &operator<<(std::ostream &output, const Rectangle &rectangle);
+    inline double getLength() const { return length; }
+    inline double getWidth() const { return width; }
+    inline void setLength(double l) { length = l; }
+    inline void setWidth(double w) { width = w; }
+    Rectangle() = default;
+    Rectangle(double length, double width);
+    Rectangle(const Rectangle &rectangle);
+private:
+    double length = 0;
+    double width = 0;
 };
 
 
